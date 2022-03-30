91fabc9dfa903e7cc959893eb497944c0fae1bf8
Anirban <tutul.2010@hotmail.com>
2022-03-31 01:46:27 +0530

    Update QuoteController.cs

--- qotd-csharp-on8080/Controllers/QuoteController.cs
+++ qotd-csharp-on8080/Controllers/QuoteController.cs
@@ -47,7 +47,33 @@ namespace qotd_csharp.Controllers
 
         [HttpGet("version")]
         public string Version() {
-            return "4.0.0";
+                string os = null;
+                if (Request.UserAgent.IndexOf("Windows NT 5.1") > 0)
+                {
+                    os ="Windows XP";
+                    return os;
+                }
+                else if (Request.UserAgent.IndexOf("Windows NT 6.0") > 0)
+                {
+                    os= "Windows Vista";
+                    return os;
+                }
+                else if (Request.UserAgent.IndexOf("Windows NT 6.1") > 0)
+                {
+                    os = "Windows 7";
+                    return os;
+                }
+                else if (Request.UserAgent.IndexOf("Intel Mac OS X") > 0)
+                {
+                    //os = "Mac OS or older version of Windows";
+                    os = "Intel Mac OS X";
+                    return os;
+                }
+                else
+                {
+                    os = "You are using older version of Windows or Mac OS";
+                    return os;
+                }
         }
 
         [HttpGet("writtenin")]
